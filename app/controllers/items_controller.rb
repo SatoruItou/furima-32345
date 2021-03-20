a1d1596347abf07ccfdeda6d22397a5d8e34058d
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-03-20 15:17:33 +0900

    出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
+  end
+
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+ 
+  private
+
+  def item_params
+   params.require(:item).permit(:nickname, user_ids: [])
   end
 end
