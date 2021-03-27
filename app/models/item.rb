5afe971829a5e3758fad64b1bde35c7e4bc50687
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-03-27 18:49:56 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,10 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
-    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
+    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
     validates :image
   end
-  
+
   belongs_to :user
   has_one_attached :image
 
@@ -15,8 +15,8 @@ class Item < ApplicationRecord
   belongs_to :scheduled
   belongs_to :category
   belongs_to :shipping
-  
-  with_options numericality: { other_than: 1} do
+
+  with_options numericality: { other_than: 1 } do
     validates :sales_id
     validates :prefecture_id
     validates :scheduled_id
