53085c110e4061230143e2766bb81897c663295b
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-04-06 23:54:56 +0900

    購入画面の購入情報出力

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,26 @@
 class PurchasesController < ApplicationController
 
   def index
-    @purchase = Purchase.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @purchase_address = PurchaseAddress.new(address_params)
+  end
+
+  def create
+    
+    @purchase_address = PurchaseAddress.new(address_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render 'purchases/index'
+    end
+  end
+
+  private
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number )
   end
 end
