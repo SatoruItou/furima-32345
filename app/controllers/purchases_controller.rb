0678f000f9a681f6e7e7f86e1aa6f6e55039eade
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-04-08 21:40:08 +0900

    エラーの表出

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,26 +1,27 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
   def index
     @item = Item.find(params[:item_id])
   end
 
   def new
+    
     @purchase_address = PurchaseAddress.new(address_params)
   end
 
   def create
-    
+    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(address_params)
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
     else
-      render 'purchases/index'
+      render action: :index
     end
   end
 
   private
   def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number )
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number )
   end
 end
