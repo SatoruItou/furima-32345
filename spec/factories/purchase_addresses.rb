76b3631f2e04eeedc1a245d93fa1d5ac8d248f55
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-04-11 21:45:22 +0900

    決済処理のテストコード

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -3,8 +3,9 @@ FactoryBot.define do
     postal_code { '123-4567' }
     prefecture_id { 3 }
     city { '東京市' }
-    address {'青山1-1'}
+    address { '青山1-1' }
     building { '東京ハイツ' }
     phone_number { '09012345678' }
+    token { 'tok_1234' }
   end
 end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
-  describe '住所情報の保存' do
+  describe '住所情報と購入履歴の保存' do
     before do
       @purchase_address = FactoryBot.build(:purchase_address)
     end
@@ -39,12 +39,12 @@ RSpec.describe PurchaseAddress, type: :model do
       it '市区町村が半角英数字だと保存されないこと' do
         @purchase_address.city = 'test'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("City is invalid. Input full-width characters")
+        expect(@purchase_address.errors.full_messages).to include('City is invalid. Input full-width characters')
       end
       it '市区町村が全角英数字だと保存されないこと' do
         @purchase_address.city = 'TEST'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("City is invalid. Input full-width characters")
+        expect(@purchase_address.errors.full_messages).to include('City is invalid. Input full-width characters')
       end
       it '番地を入力しないと保存されないこと' do
         @purchase_address.address = ''
@@ -58,10 +58,22 @@ RSpec.describe PurchaseAddress, type: :model do
         expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
       end
 
-      it '電話番号が全角英数字だと保存できないこと'do 
-      @purchase_address.phone_number = '０９０１２３４５６７８'
-      @purchase_address.valid?
-      expect(@purchase_address.errors.full_messages).to include('Phone number is invalid, Input your phone number')
+      it '電話番号が全角英数字だと保存できないこと' do
+        @purchase_address.phone_number = '０９０１２３４５６７８'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include('Phone number is invalid, Input your phone number')
+      end
+    end
+    context '購入履歴が保存できる場合' do
+      it 'tokenが入力されていると購入履歴が保存される' do
+        expect(@purchase_address).to be_valid
+      end
+    end
+    context '購入履歴が保存されない場合' do
+      it 'tokenが入力されていないと保存できないこと' do
+        @purchase_address.token = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
