62f4836ad2ff33458fd3665f97b3fbee3d7ec32b
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-05-04 18:53:58 +0900

    SNS認証・Facebook環境変数設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:favebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と数字の両方を含めて設定してください',
                                  length: { minimum: 6 }
@@ -19,4 +19,17 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+   sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+   user = User.where(email: auth.info.email).first_or_initialize(
+     nickname: auth.info.name,
+        email: auth.info.email
+   )
+   if user.persisted?
+    sns.user = user
+    sns.save
+   end
+   { user: user, sns: sns }
+  end
 end
